4c1693e0cff55c7ad11e915dd6087388186d8da4
Nicoff-0 <nicolashoyostareas@gmail.com>
2024-08-16 11:43:10 -0500

    Registro

--- Interfaz/Registro.Designer.cs
+++ Interfaz/Registro.Designer.cs
@@ -32,11 +32,14 @@
             this.TbPassword = new System.Windows.Forms.TextBox();
             this.TbMail = new System.Windows.Forms.TextBox();
             this.TbLastName = new System.Windows.Forms.TextBox();
+            this.BtRegistrar = new System.Windows.Forms.Button();
+            this.LinkIniciar = new System.Windows.Forms.LinkLabel();
+            this.TbId = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // TbName
             // 
-            this.TbName.Location = new System.Drawing.Point(12, 207);
+            this.TbName.Location = new System.Drawing.Point(169, 121);
             this.TbName.Name = "TbName";
             this.TbName.Size = new System.Drawing.Size(359, 22);
             this.TbName.TabIndex = 0;
@@ -44,36 +47,67 @@
             // 
             // TbPassword
             // 
-            this.TbPassword.Location = new System.Drawing.Point(12, 324);
+            this.TbPassword.Location = new System.Drawing.Point(169, 238);
             this.TbPassword.Name = "TbPassword";
             this.TbPassword.Size = new System.Drawing.Size(359, 22);
             this.TbPassword.TabIndex = 1;
             // 
             // TbMail
             // 
-            this.TbMail.Location = new System.Drawing.Point(12, 284);
+            this.TbMail.Location = new System.Drawing.Point(169, 198);
             this.TbMail.Name = "TbMail";
             this.TbMail.Size = new System.Drawing.Size(359, 22);
             this.TbMail.TabIndex = 2;
             // 
             // TbLastName
             // 
-            this.TbLastName.Location = new System.Drawing.Point(12, 246);
+            this.TbLastName.Location = new System.Drawing.Point(169, 160);
             this.TbLastName.Name = "TbLastName";
             this.TbLastName.Size = new System.Drawing.Size(359, 22);
             this.TbLastName.TabIndex = 3;
             this.TbLastName.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
             // 
-            // Form1
+            // BtRegistrar
+            // 
+            this.BtRegistrar.Location = new System.Drawing.Point(272, 294);
+            this.BtRegistrar.Name = "BtRegistrar";
+            this.BtRegistrar.Size = new System.Drawing.Size(110, 23);
+            this.BtRegistrar.TabIndex = 4;
+            this.BtRegistrar.Text = "Registrarse";
+            this.BtRegistrar.UseVisualStyleBackColor = true;
+            this.BtRegistrar.Click += new System.EventHandler(this.BtRegistrar_Click);
+            // 
+            // LinkIniciar
+            // 
+            this.LinkIniciar.AutoSize = true;
+            this.LinkIniciar.Location = new System.Drawing.Point(245, 342);
+            this.LinkIniciar.Name = "LinkIniciar";
+            this.LinkIniciar.Size = new System.Drawing.Size(191, 16);
+            this.LinkIniciar.TabIndex = 5;
+            this.LinkIniciar.TabStop = true;
+            this.LinkIniciar.Text = "ya tienes cuenta?, inicia secion";
+            this.LinkIniciar.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LinkIniciar_LinkClicked);
+            // 
+            // TbId
+            // 
+            this.TbId.Location = new System.Drawing.Point(169, 81);
+            this.TbId.Name = "TbId";
+            this.TbId.Size = new System.Drawing.Size(359, 22);
+            this.TbId.TabIndex = 6;
+            // 
+            // Registro
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TbId);
+            this.Controls.Add(this.LinkIniciar);
+            this.Controls.Add(this.BtRegistrar);
             this.Controls.Add(this.TbLastName);
             this.Controls.Add(this.TbMail);
             this.Controls.Add(this.TbPassword);
             this.Controls.Add(this.TbName);
-            this.Name = "Form1";
+            this.Name = "Registro";
             this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -86,5 +120,8 @@
         private System.Windows.Forms.TextBox TbPassword;
         private System.Windows.Forms.TextBox TbMail;
         private System.Windows.Forms.TextBox TbLastName;
+        private System.Windows.Forms.Button BtRegistrar;
+        private System.Windows.Forms.LinkLabel LinkIniciar;
+        private System.Windows.Forms.TextBox TbId;
     }
 }
--- Interfaz/Registro.cs
+++ Interfaz/Registro.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Hackaton_Frontasks.Clases;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +14,10 @@ namespace Hackaton_Frontasks.Interfaz
 {
     public partial class Registro : Form
     {
+        cConexion cn; //Variable cConexion
+        SqlCommand cmd; //para traer los comandos del sql
+        SqlDataAdapter da; //Se necesita para las consultas
+        DataTable dt;
         public Registro()
         {
             InitializeComponent();
@@ -25,6 +31,50 @@ namespace Hackaton_Frontasks.Interfaz
         private void textBox1_TextChanged_1(object sender, EventArgs e)
         {
 
+        }
+
+        private void BtRegistrar_Click(object sender, EventArgs e)
+        {
+            if (TbName.Text == "" || TbLastName.Text == "" || TbMail.Text == "" || TbPassword.Text == "" || TbId.Text == "" )
+            {
+                MessageBox.Show("Informacion incompleta");
+            }
+            else
+            {
+
+
+
+                cn = new cConexion(); cmd = new SqlCommand("Select * from tblUsuarios where id_usuario= '" + TbId.Text + "' ", cn.AbrirConexion());
+                da = new SqlDataAdapter(cmd);
+                dt = new DataTable();
+                da.Fill(dt);//llena dt con consulta cmd
+
+                if (dt.Rows.Count > 0)
+                {
+                    MessageBox.Show("Usuario ya registrado, inicie sesion");
+                    this.Hide();
+                    LoginPane frmy = new LoginPane();
+                    frmy.Show();
+                }
+                else
+                {
+                    cn = new cConexion();
+                    cmd = new SqlCommand("insert into tblUsuarios values ( '" + TbId.Text + "', '" + TbName.Text + "', '" + TbLastName.Text + "', '" + TbMail.Text + "', '" + TbPassword.Text + "')", cn.AbrirConexion());
+
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Usuario creado");
+                }
+                LoginPane frm = new LoginPane();
+                frm.Show();
+                this.Hide();
+            }
+        }
+
+        private void LinkIniciar_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.Hide();
+            LoginPane frm = new LoginPane();
+            frm.Show();
         }
     }
 }
